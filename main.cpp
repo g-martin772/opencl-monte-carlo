4172e76ea50e0d2e725286bd6eedc6f226a25ee3
gabriel <g.martin@htlkrems.at>
2025-05-13 13:52:08 +0200

    Setup opencl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
+#include <CL/cl.hpp>
+
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#ifndef DEVICE
+#define DEVICE CL_DEVICE_TYPE_DEFAULT
+#endif
+
+std::string load_program(std::string input) {
+    std::ifstream stream(input.c_str());
+    if (!stream.is_open()) {
+        std::cout << "Cannot open file: " << input << std::endl;
+        exit(1);
+    }
+    return std::string(std::istreambuf_iterator<char>(stream),
+                       (std::istreambuf_iterator<char>()));
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cl::Context context(DEVICE);
+
+    // load in kernel source, creating a program object for the context
+    cl::Program program(context, load_program("../kernel.cl"), true);
+
+    // get the command queue
+    cl::CommandQueue queue(context);
+
+    // create the kernel functor
+    auto vadd =
+        cl::KernelFunctor<cl::Buffer, cl::Buffer, cl::Buffer, int>(program, "vadd");
+
+
     return 0;
 }
